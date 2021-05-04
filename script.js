41fc6038abb520f536a034322fc260f89651021b
plineburg <61601657+plineburg@users.noreply.github.com>
2021-05-03 21:44:22 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate/100;
+    
+
+    if (document.getElementById("principal").value == "") {
+        alert("Please enter the principal amount");
+        document.getElementById("principal").focus();
+        return false;
+    } else if(document.getElementById("principal").value <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    }
+
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal+"\</mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\</mark\>\<br\>You will receive an amount of \<mark\>"+interest+"\</mark\>,\<br\>in the year "+year+"\<br\>";
 
@@ -18,9 +30,3 @@ function updateRate() {
         document.getElementById("rate_val").innerText=rateval;
     }
 
-function form_validation() {
-    const amount = document.getElementById("principle");
-    if (amount <= 1) {
-        alert("Enter a positive number")
-    }
-}
